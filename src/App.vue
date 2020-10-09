17b04f02f07c7a6c41d13b5eff67ce2d94b58639
Stephanie Thornley <sthornley00@gmail.com>
2020-10-09 15:37:10 +0100

    Set up AWS Amplify and Cognito

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Undercover Elf</h1>
+    <amplify-authenticator v-if="authState !== 'signedin'">
+      <amplify-sign-up
+        header-text="Create Account"
+        slot="sign-up"
+        username-alias="email"
+        :form-fields.prop="formFieldsSignUp"
+      ></amplify-sign-up>
+      <amplify-sign-in slot="sign-in" username-alias="email" :form-fields.prop="formFieldsSignIn"></amplify-sign-in>
+      <div v-if="authState === 'signedin'">
+        <amplify-sign-out></amplify-sign-out>
+        <div>Hello, "curlybracketsx2 user.username"</div>
+      </div>
+    </amplify-authenticator>
+
+    <Home v-bind:name="name" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Home
+  },
+  data() {
+    return {
+      authState: "",
+      formFieldsSignUp: [
+        {
+          type: "name",
+          label: "Full name *",
+          placeholder: "e.g. Kris Kringle",
+          required: true
+        },
+        {
+          type: "email",
+          label: "Email *",
+          placeholder: "kris@northpole.com",
+          required: true
+        },
+        { type: "password", label: "Password *", required: true }
+      ],
+      formFieldsSignIn: [
+        {
+          type: "email",
+          label: "Email",
+          placeholder: "kris@northpole.com",
+          required: true
+        },
+        { type: "password", label: "Password", required: true }
+      ],
+      name: "Stephanie"
+    };
   }
-}
+};
 </script>
 
 <style>
+html {
+  background-color: rgb(128, 228, 181);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
