5d7195308574cefc8905efbb23b3a7d785d9910e
Stephanie Thornley <sthornley00@gmail.com>
2021-03-09 15:55:48 +0000

    Refactor group page to use data store instead of hardcoded

--- src/components/GroupPage.vue
+++ src/components/GroupPage.vue
@@ -2,23 +2,23 @@
   <div>
     <div class="top-of-page">
       <img src="../assets/gift-bag.svg" id="logo" width="50rem" />
-      <h2>{{ groupName }}</h2>
+      <h2>{{ group.name }}</h2>
       <button
         class="settings"
-        v-if="admin"
+        v-if="group.admin"
         v-on:click="$router.push(`/groups/edit?groupId=${groupId}`)"
       >
         <span>
           <i class="fas fa-cog"></i>
         </span>
       </button>
-      <button id="leave-group-button" v-if="!admin && !namesDrawn">Leave group</button>
-      <div id="invite-info" v-if="admin">
+      <button id="leave-group-button" v-if="!group.admin && !group.closed">Leave group</button>
+      <div id="invite-info" v-if="group.admin">
         <h3>Invitation ID:</h3>
-        <p>{{ invitationId }}</p>
+        <p>{{ groupId }}</p>
 
         <h3>Invitation link:</h3>
-        <p>{{ `https://undercover-elf-demo.com/groups/join?id=${invitationId}` }}</p>
+        <p>{{ `https://undercover-elf-demo.com/groups/join?id=${groupId}` }}</p>
       </div>
 
       <router-link
@@ -28,31 +28,31 @@
       <div id="group-info">
         <div>
           <h3>Exchange date:</h3>
-          <p>{{ exchange }}</p>
+          <p>{{ group.exchange }}</p>
         </div>
         <div>
           <h3>Budget:</h3>
-          <p>{{ budget }}</p>
+          <p>{{ group.budget }}</p>
         </div>
         <div>
           <h3>Group admin:</h3>
-          <p>{{ adminName }}</p>
+          <p>{{ group.adminName }}</p>
         </div>
         <div>
           <h3>You are buying for:</h3>
           <div>
-            <p v-if="namesDrawn">{{ buyingFor }}</p>
-            <p v-if="!namesDrawn">Names have not been drawn yet</p>
+            <p v-if="group.closed">{{ group.buyingFor }}</p>
+            <p v-if="!group.closed">Names have not been drawn yet</p>
             <router-link
               :to="`/wishlist/r2888e66cla9?groupId=${groupId}`"
-              v-if="namesDrawn"
-            >View {{ buyingFor }}'s wishlist</router-link>
+              v-if="group.closed"
+            >View {{ group.buyingFor }}'s wishlist</router-link>
           </div>
         </div>
 
         <h3>Group members:</h3>
         <ul>
-          <li v-for="member in members" :key="member">{{ member }}</li>
+          <li v-for="member in group.members" :key="member">{{ member }}</li>
         </ul>
       </div>
     </div>
@@ -60,82 +60,23 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
+
 export default {
   name: "GroupPage",
-  created() {
-    if (this.$route.params.groupId === "dum813d0r3s4rmy") {
-      this.groupName = "Dumbledore's Army";
-      this.invitationId = "dum813d0r3s4rmy";
-      this.budget = this.dumbledoresArmy.budget;
-      this.exchange = this.dumbledoresArmy.exchange;
-      this.admin = true;
-      this.adminName = this.dumbledoresArmy.adminName;
-      this.namesDrawn = this.dumbledoresArmy.namesDrawn;
-      this.members = this.dumbledoresArmy.members;
-      this.buyingFor = this.dumbledoresArmy.buyingFor;
-    } else {
-      this.groupName = "Gryffindor Quidditch Team";
-      this.invitationId = "qu1dd1tch";
-      this.budget = this.gryffindorQuidditchTeam.budget;
-      this.exchange = this.gryffindorQuidditchTeam.exchange;
-      this.admin = false;
-      this.adminName = this.gryffindorQuidditchTeam.adminName;
-      this.namesDrawn = this.gryffindorQuidditchTeam.namesDrawn;
-      this.members = this.gryffindorQuidditchTeam.members;
-      this.buyingFor = this.gryffindorQuidditchTeam.buyingFor;
-    }
-  },
   computed: {
+    ...mapState("demo", ["groups"]),
+    ...mapGetters("demo", ["getGroup"]),
     groupId() {
       return this.$route.params.groupId;
     }
   },
+  created() {
+    this.group = this.getGroup(this.groupId);
+  },
   data() {
     return {
-      dumbledoresArmy: {
-        exchange: "10/12/95",
-        budget: "3 Galleons",
-        adminName: "Harry Potter",
-        buyingFor: "Luna Lovegood",
-        namesDrawn: true,
-        members: [
-          "Harry Potter",
-          "Padma Patil",
-          "Parvarti Patil",
-          "Hannah Abbott",
-          "Lavender Brown",
-          "Katie Bell",
-          "Susan Bones",
-          "Cho Chang",
-          "Colin Creevey",
-          "Marietta Edgecombe",
-          "Justin Finch-Fletchley",
-          "Hermione Granger",
-          "Angelina Johnson",
-          "Lee Jordan",
-          "Neville Longbottom",
-          "Luna Lovegood",
-          "Fred Weasley",
-          "George Weasley",
-          "Ginny Weasley",
-          "Ron Weasley"
-        ]
-      },
-      gryffindorQuidditchTeam: {
-        exchange: "15/12/91",
-        budget: "2 Galleons",
-        adminName: "Oliver Wood",
-        namesDrawn: false,
-        buyingFor: "Fred Weasley",
-        members: [
-          "Fred Weasley",
-          "George Weasley",
-          "Angelina Johnson",
-          "Alicia Spinnet",
-          "Harry Potter",
-          "Oliver Wood"
-        ]
-      }
+      group: {}
     };
   }
 };
--- src/store/modules/demo.js
+++ src/store/modules/demo.js
@@ -7,12 +7,62 @@ const state = {
     {
       name: "Dumbledore's Army",
       groupId: "dum813d0r3s4rmy",
+      exchange: "10/12/95",
+      budget: "3 Galleons",
+      admin: true,
+      adminName: "Harry Potter",
+      buyingFor: "Luna Lovegood",
+      closed: true,
+      members: [
+        "Harry Potter",
+        "Padma Patil",
+        "Parvarti Patil",
+        "Hannah Abbott",
+        "Lavender Brown",
+        "Katie Bell",
+        "Susan Bones",
+        "Cho Chang",
+        "Colin Creevey",
+        "Marietta Edgecombe",
+        "Justin Finch-Fletchley",
+        "Hermione Granger",
+        "Angelina Johnson",
+        "Lee Jordan",
+        "Neville Longbottom",
+        "Luna Lovegood",
+        "Fred Weasley",
+        "George Weasley",
+        "Ginny Weasley",
+        "Ron Weasley",
+      ],
     },
-    { name: "Gryffindor Quidditch Team", groupId: "qu1dd1tch" },
+    {
+      name: "Gryffindor Quidditch Team",
+      groupId: "qu1dd1tch",
+      exchange: "15/12/91",
+      budget: "2 Galleons",
+      admin: false,
+      adminName: "Oliver Wood",
+      closed: false,
+      buyingFor: "Fred Weasley",
+      members: [
+        "Fred Weasley",
+        "George Weasley",
+        "Angelina Johnson",
+        "Alicia Spinnet",
+        "Harry Potter",
+        "Oliver Wood",
+      ],
+    },
   ],
 };
 
-const getters = {};
+const getters = {
+  getGroup: (state) => (groupId) => {
+    let groupToGet = state.groups.find((group) => group.groupId == groupId);
+    return groupToGet;
+  },
+};
 
 const mutations = {};
 
